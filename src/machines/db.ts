b1996589beda0b6dcfe5d77efe2a696cef7441e4
Johannes <johtso@gmail.com>
2023-02-20 20:38:11 +0000

    Update db.ts

--- src/machines/db.ts
+++ src/machines/db.ts
@@ -31,7 +31,7 @@ async function initWorker() {
           serverMode: "full", // file is just a plain old full sqlite database
           requestChunkSize: 4096, // the page size of the  sqlite database (by default 4096)
           // url: (__MODE__ === "production") ? "https://data.infinitenovelty.com/file/iabi-data/db.3.sqlite" : "db.sqlite"
-          url: "https://data.infinitenovelty.com/db.4.sqlite"
+          url: "https://cached.infinitenovelty.com/db.4.sqlite"
           // url: "/db.4.sqlite"
         }
       }
